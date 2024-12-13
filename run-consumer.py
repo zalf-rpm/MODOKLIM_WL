5b22c0124e3d0d6e2b27c0ae06b10feec14c820b
Michael Berg-Mohnicke <michael.berg@zalf.de>
2024-12-13 12:54:17 +0100

    update

--- run-consumer.py
+++ run-consumer.py
@@ -88,10 +88,8 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
     make_dict_nparr = lambda: defaultdict(lambda: np.full((ncols,), -9999, dtype=np.float))
 
     output_grids = {
-        "Pot_ET_06-15": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "Pot_ET_08-15": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-        "Pot_ET_10-15": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
-
+        "TUA": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
+        "ODY": {"data": make_dict_nparr(), "cast-to": "float", "digits": 1},
     }
     output_keys = list(output_grids.keys())
 
