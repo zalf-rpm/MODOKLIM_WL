4b68253a61ef9c65d5412069a5d3e3d139627753
ssuren55 <Surendra.Giri@zalf.de>
2025-06-20 09:41:36 +0200

    change

--- run-consumer.py
+++ run-consumer.py
@@ -95,20 +95,7 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
     # }
 
     output_grids = {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        "TimeUnderAnoxia": {"data": make_dict_nparr(), "cast-to": "float", "digits": 3}
-        
-        # "OxygenDeficit": {"data": make_dict_nparr(), "cast-to": "float", "digits": 3}
-=======
-        # "TimeUnderAnoxia": {"data": make_dict_nparr(), "cast-to": "float", "digits": 3},
-        "OxygenDeficit": {"data": make_dict_nparr(), "cast-to": "float", "digits": 3}
->>>>>>> parent of 8438edf (Update run-consumer.py)
-=======
-        "TimeUnderAnoxia": {"data": make_dict_nparr(), "cast-to": "float", "digits": 3},
-        # "OxygenDeficit": {"data": make_dict_nparr(), "cast-to": "float", "digits": 3}
->>>>>>> parent of 41c7cfa (change)
-        
+        "TimeUnderAnoxia": {"data": make_dict_nparr(), "cast-to": "float", "digits": 3}   
     }
 
 
