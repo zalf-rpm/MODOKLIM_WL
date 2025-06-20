768c52fff22fa14f07dfce24a9a41fc431fb8a11
ssuren55 <Surendra.Giri@zalf.de>
2025-06-20 11:51:32 +0200

    Update run-producer.py

--- run-producer.py
+++ run-producer.py
@@ -86,7 +86,7 @@ DATA_GRID_SLOPE = "germany/MOL-slop-wr_100_25832_etrs89-utm32n.asc"
 DATA_GRID_LAND_USE = "germany/landuse_1000_31469_gk5.asc"
 DATA_GRID_SOIL = "germany/MOL-buek-wr_100_25832_etrs89-utm32n.asc"
 DATA_GRID_SOIL_OW = "germany/buek200_1000_25832_etrs89-utm32n_OW.asc"
-DATA_GRID_CROPS = "germany/MOLLLL-crop-wr_100_25832_etrs89-utm32n.asc"
+DATA_GRID_CROPS = "germany/MOL-crop-wr_100_25832_etrs89-utm32n.asc"
 
 #TEMPLATE_PATH_CLIMATE_CSV = "{gcm}/{rcm}/{scenario}/{ensmem}/{version}/row-{crow}/col-{ccol}.csv"
 TEMPLATE_PATH_CLIMATE_CSV = "{crow}/daily_mean_RES1_C{ccol}R{crow}.csv.gz"
